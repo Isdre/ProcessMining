d5715d577d33ab47ec1bfd9040579565c9a686ce
jGrzyb <grzybjaku@gmail.com>
2025-01-21 21:45:22 +0100

    use_vampire on all

--- use_vampire.py
+++ use_vampire.py
@@ -4,8 +4,21 @@ import os
 # os.chdir("D:\\JetBrains\\Projects\\StudioProject")
 # script_path = 'D:\\JetBrains\\Projects\\StudioProject\\vampire'
 
-script_path = os.getcwd() + '/vampire'
-result = subprocess.run([script_path, 'problem.p'], capture_output=True, text=True)
+# script_path = os.getcwd() + '/vampire'
+# result = subprocess.run([script_path, 'problem.p'], capture_output=True, text=True)
 
-print(f"Output:\n{result.stdout}")
-print(f"Errors:\n{result.stderr}")
+# print(f"Output:\n{result.stdout}")
+# print(f"Errors:\n{result.stderr}")
+
+import subprocess
+import os
+
+script_path = os.getcwd() + '/vampire'
+
+for root, dirs, files in os.walk('problems'):
+    for file in files:
+        file_path = os.path.join(root, file)
+        result = subprocess.run([script_path, file_path], capture_output=True, text=True)
+        print(f"-------- Folder: {os.path.basename(root)}, File: {file} --------")
+        print(f"\n{result.stdout}")
+        print(f"\n{result.stderr}")
