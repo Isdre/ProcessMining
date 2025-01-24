a87391c2d9cecd4843bcce2134911a48884edc05
jGrzyb <grzybjaku@gmail.com>
2025-01-24 21:03:27 +0100

    example usage repaired

--- example_usage.py
+++ example_usage.py
@@ -6,6 +6,7 @@ from Julia import tree_to_tptp
 from create_thesis_with_2_spec import LogicFunctions, create_thesis_with_2_spec
 
 def use_provers(file_name):
+    vampire_result = "No result"
     try:
         # use vampire
         script_path = os.getcwd() + '/vampire'
@@ -13,26 +14,29 @@ def use_provers(file_name):
         print(f"Output:\n{result.stdout}")
         print(f"Errors:\n{result.stderr}")
         vampire_result = [line for line in result.stdout.splitlines() if line.startswith('% SZS')][0]
-
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Vampire not fond or did not work properly")
+        print("HINT: Check prover path")
+    
+    e_prover_result = "No result"
+    try:
         # use E prover
         script_path = os.getcwd() + "/E/PROVER/eprover-ho"
         result = subprocess.run([script_path, file_name], capture_output=True, text=True, env=os.environ)
         print(f"Output:\n{result.stdout}")
         print(f"Errors:\n{result.stderr}")
         e_prover_result = [line for line in result.stdout.splitlines() if line.startswith('# SZS')][0]
-
-        # Print results
-        print(f"\n\n")
-        print(f"Vampire result: {vampire_result}")
-        print(f"E prover result: {e_prover_result}")
     except Exception as e:
         print(f"Error: {e}")
-        print("Vampire or E-prover not fond or did not work properly")
-        print("HINT: Check prover paths")
+        print("E-prover not fond or did not work properly")
+        print("HINT: Check prover path")
+    
+    return f'{file_name}\nVampire: {vampire_result}\nE-Prover: {e_prover_result}\n'
 
 
 if __name__ == "__main__":
-    log = pm4py.format_dataframe(pd.read_csv("repairExample.csv", sep=','), case_id='Case ID', activity_key='Activity', timestamp_key='Start Timestamp')
+    log = pm4py.format_dataframe(pd.read_csv("Data/repairExample.csv", sep=','), case_id='Case ID', activity_key='Activity', timestamp_key='Start Timestamp')
 
     process_tree = pm4py.discover_process_tree_inductive(log,0.25, activity_key='concept:name', case_id_key='case:concept:name', timestamp_key='time:timestamp')
     pm4py.view_process_tree(process_tree)
@@ -44,5 +48,5 @@ if __name__ == "__main__":
 
     create_thesis_with_2_spec("problem25.p", "problem75.p", "problem_implied_by.p", LogicFunctions.IMPLIED_BY)
 
-    use_provers("problem25.p")
-    use_provers("problem_implied_by.p")
+    print(use_provers("problem25.p"))
+    print(use_provers("problem_implied_by.p"))
