1dc7e45816f531db9322eba770444c0df5328beb
jGrzyb <grzybjaku@gmail.com>
2025-01-24 20:32:31 +0100

    if main in example_usage

--- example_usage.py
+++ example_usage.py
@@ -31,18 +31,18 @@ def use_provers(file_name):
         print("HINT: Check prover paths")
 
 
+if __name__ == "__main__":
+    log = pm4py.format_dataframe(pd.read_csv("repairExample.csv", sep=','), case_id='Case ID', activity_key='Activity', timestamp_key='Start Timestamp')
 
-log = pm4py.format_dataframe(pd.read_csv("repairExample.csv", sep=','), case_id='Case ID', activity_key='Activity', timestamp_key='Start Timestamp')
+    process_tree = pm4py.discover_process_tree_inductive(log,0.25, activity_key='concept:name', case_id_key='case:concept:name', timestamp_key='time:timestamp')
+    pm4py.view_process_tree(process_tree)
+    tree_to_tptp(process_tree, "problem25.p", rule_prefix="A")
 
-process_tree = pm4py.discover_process_tree_inductive(log,0.25, activity_key='concept:name', case_id_key='case:concept:name', timestamp_key='time:timestamp')
-pm4py.view_process_tree(process_tree)
-tree_to_tptp(process_tree, "problem25.p", rule_prefix="A")
+    process_tree = pm4py.discover_process_tree_inductive(log,0.75, activity_key='concept:name', case_id_key='case:concept:name', timestamp_key='time:timestamp')
+    pm4py.view_process_tree(process_tree)
+    tree_to_tptp(process_tree, "problem75.p", rule_prefix="B")
 
-process_tree = pm4py.discover_process_tree_inductive(log,0.75, activity_key='concept:name', case_id_key='case:concept:name', timestamp_key='time:timestamp')
-pm4py.view_process_tree(process_tree)
-tree_to_tptp(process_tree, "problem75.p", rule_prefix="B")
+    create_thesis_with_2_spec("problem25.p", "problem75.p", "problem_implied_by.p", LogicFunctions.IMPLIED_BY)
 
-create_thesis_with_2_spec("problem25.p", "problem75.p", "problem_implied_by.p", LogicFunctions.IMPLIED_BY)
-
-use_provers("problem25.p")
-use_provers("problem_implied_by.p")
+    use_provers("problem25.p")
+    use_provers("problem_implied_by.p")
